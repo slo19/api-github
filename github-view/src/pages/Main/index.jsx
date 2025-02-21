245c58781f40672e4f3a6369db80e874feafb493
slo19 <saulo.miranda19@gmail.com>
2025-02-21 14:26:51 -0300

    prettier applied

--- github-view/src/pages/Main/index.jsx
+++ github-view/src/pages/Main/index.jsx
@@ -1,27 +1,26 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { MdSearch } from "react-icons/md";
 
 import { Container, Form, Logo, Title, Input, Button } from "./styles";
 import githubLogo from "../../assets/images/github-logo.svg";
-import {theme} from "../../styles/Theme";
+import { theme } from "../../styles/Theme";
 
-export default function Main () {
-  const [login, setLogin] = useState('');
+export default function Main() {
+  const [login, setLogin] = useState("");
   return (
     <Container>
-      <Logo
-        src={githubLogo}
-        alt="GitHub Logo"
-      />
-      <Title>
-        API GitHub
-      </Title>
+      <Logo src={githubLogo} alt="GitHub Logo" />
+      <Title>API GitHub</Title>
       <Form>
-        <Input placeholder="Usuario" value={login} onChange={(event) => setLogin(event.target.value)}/>
-        <Button to={`/${login}/repositories`}><MdSearch size={42} color={theme.colors.text}/></Button>
+        <Input
+          placeholder="Usuario"
+          value={login}
+          onChange={(event) => setLogin(event.target.value)}
+        />
+        <Button to={`/${login}/repositories`}>
+          <MdSearch size={42} color={theme.colors.text} />
+        </Button>
       </Form>
-
     </Container>
   );
 }
-
