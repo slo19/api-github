2529fd27e3174e32d694b8d555d983fc49e64ca9
slo19 <saulo.miranda19@gmail.com>
2025-03-14 15:35:55 -0300

    add filter

---
+++ github-view/src/pages/RepositoriesPage/Filter/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container, Selector, Cleaner } from "./styles";
+
+function Filter() {
+  const langs = [
+    { name: "C#", count: 5, color: "#f1c40f" },
+    { name: "TypeScript", count: 2, color: "#344fff" },
+    { name: "Go", count: 3, color: "#3498db" },
+  ];
+
+  const selectors = langs.map(({ name, count, color }) => (
+    <Selector key={name.toLowerCase} color={color}>
+      <span>{name}</span>
+      <span>{count}</span>
+    </Selector>
+  ));
+  return (
+    <Container>
+      {selectors}
+      <Cleaner>Limpar</Cleaner>
+    </Container>
+  );
+}
+
+export default Filter;
